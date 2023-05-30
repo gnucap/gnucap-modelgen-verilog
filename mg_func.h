2931a51ce8b2da9ea598fc6bcd3c57a50b0cd7c1
Felix Salfelder <felix@salfelder.org>
2023-05-30 00:00:00 +0000

    missing file

---
+++ mg_func.h
@@ -0,0 +1,38 @@
+/*                        -*- C++ -*-
+ * Copyright (C) 2023 Felix Salfelder
+ * Author: Felix Salfelder
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *------------------------------------------------------------------
+ * Verilog-AMS builtin functions.. under construction
+ */
+
+#include <u_function.h>
+
+class MGVAMS_FUNCTION : public FUNCTION {
+  std::string eval(CS&, const CARD_LIST*)const override{
+	  unreachable();
+	  return "AAA";
+  }
+public:
+  virtual int arity() const = 0;
+  virtual std::string code_name() const{
+	  unreachable();
+	  return "";
+  }
+};
