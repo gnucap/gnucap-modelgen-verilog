e20cb4bf49c9f52a3ad3f24068272d8b12fe2bf5
Felix Salfelder <felix@salfelder.org>
2025-07-23 00:00:00 +0000

    add the other $rdist_*

--- mgvams/mg_task_random.cc
+++ mgvams/mg_task_random.cc
@@ -34,12 +34,14 @@ namespace{
 class RDIST : public MGVAMS_TASK {
 private:
   std::string _what;
+  std::string _args;
 public:
-  explicit RDIST(std::string const& what) : MGVAMS_TASK(), _what(what) {
+  explicit RDIST(std::string const& what, std::string const& args)
+    : MGVAMS_TASK(), _what(what), _args(args) {
     set_label("$rdist_"+what);
   }
 protected:
-  RDIST(RDIST const& p) : MGVAMS_TASK(p), _what(p._what) {}
+  RDIST(RDIST const& p) : MGVAMS_TASK(p), _what(p._what), _args(p._args) {}
   MGVAMS_TASK* clone()const override {return new RDIST(*this);}
 private:
   bool is_common()const override {return true;} // extra CARD*
@@ -57,11 +59,31 @@ private:
   Token* new_token(Module&, size_t)const override {
    return nullptr; // leave it to m;
   }
+  void argnames(std::ostream& o)const {
+    int i = 0;
+    for(char _ : _args){
+      o << ", a" << i++;
+    }
+  }
+  void voidargs(std::ostream& o)const {
+    int i = 0;
+    for(char _ : _args){
+      o << " (void) a" << i++ << ";";
+    }
+  }
   void args(std::ostream& o, bool names=true)const {
-    if(names){
-      o << "int32_t seed, double const& l, double const& u";
-    }else{
-      o << "int32_t, double const&, double const&";
+    int i = 0;
+    for(char a : _args){
+      switch(a){
+      case 'i': o << ", int"; break;
+      case 'd': o << ", double"; break;
+      default: unreachable();
+      }
+      o << " const&";
+      if(names) {
+	o << " a" << i++;
+      }else{
+      }
     }
   }
   void make_cc_dev(std::ostream& o)const override {
@@ -69,84 +91,84 @@ private:
     o____ "double _value{0};\n";
     o____ "int32_t _seed{0};\n";
 
-    o__ "double precalc(CARD*, int32_t& seed, double l, double u) {\n";
-    o____ "trace3(\"random::precalc\", CKT_BASE::_sim->_time0, seed, _value);\n";
-    o____ "(void)seed; (void)l; (void)u;\n";
-    o____ "return _value;\n";
-    o__ "}\n";
+    o____ "double precalc(CARD*, int32_t& seed"; args(o); o << ") {\n";
+    o______ "trace3(\"random::precalc\", CKT_BASE::_sim->_time0, seed, _value);\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o__ "double precalc(CARD*, int32_t const& seed, double l, double u) {\n";
-    o____ "trace3(\"random::precalc1\", CKT_BASE::_sim->_time0, seed, _value);\n";
-    o____ "(void)seed; (void)l; (void)u;\n";
-    o____ "return _value;\n";
-    o__ "}\n";
+    o____ "double precalc(CARD*, int32_t const& seed"; args(o); o << "){\n";
+    o______ "trace3(\"random::precalc1\", CKT_BASE::_sim->_time0, seed, _value);\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o__ "double tr_initial(CARD *d, int32_t& s, double l, double u) {\n";
-    o____ "return tr_begin(d, s, l, u);\n";
-    o__ "}\n";
-    o__ "double tr_initial(CARD *d, int32_t const& s, double l, double u) {untested();\n";
-    o____ "return tr_begin(d, s, l, u);\n";
-    o__ "}\n";
+    o____ "double tr_initial(CARD *d, int32_t& s"; args(o); o << ") {\n";
+    o______ "return tr_begin(d, s"; argnames(o); o << ");\n";
+    o____ "}\n";
+    o____ "double tr_initial(CARD *d, int32_t const& s"; args(o); o << ") {untested();\n";
+    o______ "return tr_begin(d, s"; argnames(o); o << ");\n";
+    o____ "}\n";
 
-    o__ "double tr_begin(CARD*, int32_t& seed, double l, double u) {\n";
-    o____ "trace2(\"random::begin01\", seed, _value);\n";
-    o____ "_value = rdist::"+_what+"(seed, l, u);\n";
-    o____ "trace2(\"random::begin02\", seed, _value);\n";
-    o____ "return _value;\n";
-    o__ "}\n";
+    o____ "double tr_begin(CARD*, int32_t& seed"; args(o); o << ") {\n";
+    o______ "trace2(\"random::begin01\", seed, _value);\n";
+    o______ "_value = rdist::"+_what+"(seed"; argnames(o); o << ");\n";
+    o______ "trace2(\"random::begin02\", seed, _value);\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o__ "double tr_begin(CARD*, int32_t const& slot, double l, double u) {\n";
-    o____ "trace2(\"random::begin1\", slot, _value);\n";
-    o____ "_value = rdist::"+_what+"(random_seed(slot), l, u);\n";
-    o____ "return _value;\n";
-    o__ "}\n";
+    o____ "double tr_begin(CARD*, int32_t const& slot"; args(o); o << ") {\n";
+    o______ "trace2(\"random::begin1\", slot, _value);\n";
+    o______ "_value = rdist::"+_what+"(random_seed(slot)"; argnames(o); o << ");\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_eval(CARD*, int32_t const& seed, double l, double u) {\n";
-    o << "      trace3(\"random::eval\", _seed, seed, _value);\n";
-    o << "      (void)seed; (void)l; (void)u;\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_eval(CARD*, int32_t const& seed"; args(o); o << ") {\n";
+    o______ "trace3(\"random::eval\", _seed, seed, _value);\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "return _value;\n";
+    o____ "}\n";
 
 
-    o << "    double tr_review(CARD*, "; args(o); o << ") {untested();\n";
-    o << "      (void)seed; (void)l; (void)u;\n";
-    o << "      trace2(\"random::review\", seed, _value);\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_review(CARD*, int32_t const& seed"; args(o); o << ") {untested();\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "trace2(\"random::review\", seed, _value);\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_advance(MOD* d, int32_t& seed, double l, double u) {\n";
-    o << "      trace3(\"random::advance1\", CKT_BASE::_sim->_time0, seed, _value);\n";
-    o << "      d->q_accept();\n";
-    o << "      int32_t s = seed;\n";
-    o << "      _value = rdist::"+_what+"(s, l, u);\n";
-    o << "      seed = s;\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_advance(MOD* d, int32_t& seed"; args(o); o << ") {\n";
+    o______ "trace3(\"random::advance1\", CKT_BASE::_sim->_time0, seed, _value);\n";
+    o______ "d->q_accept();\n";
+    o______ "int32_t s = seed;\n";
+    o______ "_value = rdist::"+_what+"(s"; argnames(o); o << ");\n";
+    o______ "seed = s;\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_advance(MOD* d, int32_t const& slot, double l, double u) {\n";
-    o << "      trace4(\"random::advance2\", CKT_BASE::_sim->_time0, slot, _value, OPT::foooo);\n";
-    o << "      int32_t& s = random_seed(slot);\n";
-    o << "      tr_advance(d, s, l, u);\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_advance(MOD* d, int32_t const& slot"; args(o); o << ") {\n";
+    o______ "trace4(\"random::advance2\", CKT_BASE::_sim->_time0, slot, _value, OPT::foooo);\n";
+    o______ "int32_t& s = random_seed(slot);\n";
+    o______ "tr_advance(d, s"; argnames(o); o << ");\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_regress(CARD*, "; args(o); o << ") {untested();\n";
-    o << "      (void)seed; (void)l; (void)u;\n";
-    o << "      incomplete();\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_regress(CARD*,int32_t const& seed"; args(o); o << ") {untested();\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "incomplete();\n";
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_accept(CARD*, int32_t const& seed, double l, double u) {\n";
-    o << "      (void)seed; (void)l; (void)u;\n";
-    o << "      return _value;\n";
-    o << "    }\n";
+    o____ "double tr_accept(CARD*, int32_t const& seed"; args(o); o << ") {\n";
+    o______ "(void)seed;\n"; voidargs(o);
+    o______ "return _value;\n";
+    o____ "}\n";
 
-    o << "    double tr_accept(CARD*, int32_t& seed, double l, double u) {\n";
-    o << "      trace3(\"random::accept\", CKT_BASE::_sim->_time0, seed, _value);\n";
-    o << "      double new_value = rdist::"+_what+"(seed, l, u);\n";
-    o << "      // assert(_value == new_value);\n";
-    o << "      return _value = new_value;\n";
-    o << "    }\n";
+    o____ "double tr_accept(CARD*, int32_t& seed"; args(o); o << ") {\n";
+    o______ "trace3(\"random::accept\", CKT_BASE::_sim->_time0, seed, _value);\n";
+    o______ "double new_value = rdist::"+_what+"(seed"; argnames(o); o << ");\n";
+    o______ "// assert(_value == new_value);\n";
+    o______ "return _value = new_value;\n";
+    o____ "}\n";
 
     o__ "}_" << label() << ";\n";
   }
@@ -155,10 +177,20 @@ private:
   }
 };
 /*--------------------------------------------------------------------------*/
-RDIST d0("uniform");
+RDIST d0("uniform", "dd");
 DISPATCHER<FUNCTION>::INSTALL p0(&function_dispatcher, "$rdist_uniform", &d0);
-RDIST d1("normal");
+RDIST d1("normal", "dd");
 DISPATCHER<FUNCTION>::INSTALL p1(&function_dispatcher, "$rdist_normal", &d1);
+RDIST d2("exponential", "d");
+DISPATCHER<FUNCTION>::INSTALL p2(&function_dispatcher, "$rdist_exponential", &d2);
+RDIST d3("poisson", "d");
+DISPATCHER<FUNCTION>::INSTALL p3(&function_dispatcher, "$rdist_poisson", &d3);
+RDIST d4("chi_square", "i");
+DISPATCHER<FUNCTION>::INSTALL p4(&function_dispatcher, "$rdist_chi_square", &d4);
+RDIST d5("t", "i");
+DISPATCHER<FUNCTION>::INSTALL p5(&function_dispatcher, "$rdist_t", &d5);
+RDIST d6("erlangian", "dd");
+DISPATCHER<FUNCTION>::INSTALL p6(&function_dispatcher, "$rdist_erlangian", &d6);
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
 }
