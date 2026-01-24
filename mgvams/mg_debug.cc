e53d64f42c3967cb45a4b6a947546b8d929103a9
Felix Salfelder <felix@salfelder.org>
2026-01-24 00:00:00 +0000

    move $trace to debug plugin
    - inherit, share printf wrapper
    - fix integer conversion in $trace
    - adjust a few tests & refs
    
    trace/debug fuzz

--- mgvams/mg_debug.cc
+++ mgvams/mg_debug.cc
@@ -34,6 +34,7 @@ namespace{
 class DEBUG_TASK : public MGVAMS_TASK {
 public:
   explicit DEBUG_TASK() : MGVAMS_TASK(){
+    set_label("$debug");
   }
 private:
   std::string eval(CS&, const PARAM_LIST*)const override{ untested();
@@ -46,6 +47,10 @@ private:
   bool has_tr_accept()const override {return true;}
   // Token* new_token(Module&, size_t)const override{ return nullptr; }
   void make_cc_dev(std::ostream& o)const override {
+    make_cc_dev_(o, "");
+  }
+protected:
+  void make_cc_dev_(std::ostream& o, std::string const& s)const {
     if(num_args()>1){
       o____ "template<";
       std::string sep;
@@ -56,22 +61,22 @@ private:
       o << ">\n";
     }else{
     }
-    o__ "void " << label() << "(std::string const& a0";
+    o__ "void " << label() << s << "(std::string const& a0";
     for(size_t i=1; i<num_args(); ++i) {
       o << ", T" << i << " a" << i;
     }
     o << ") {\n";
-    o______ "fprintf(stdout, a0.c_str()";
+    if(s.size()){
+      o______ "std::string aa = std::string(\"@" << s << ":\") + a0;\n";
+      o______ "char const* cs = aa.c_str();\n";
+    }else{
+      o______ "char const* cs = a0.c_str();\n";
+    }
+    o______ "fprintf(stdout, cs";
     for(size_t i=1; i<num_args(); ++i) {
       o << ", plain_value(a" << i << ")";
     }
     o << ");\n";
-    o__ "}\n";
-    o__ "void " << label() << "__precalc(std::string const&";
-    for(size_t i=1; i<num_args(); ++i) {
-      o << ", double";
-    }
-    o << "){\n";
     o__ "}\n";
   }
   std::string code_name()const override{
@@ -80,6 +85,59 @@ private:
   Data_Type const* return_type()const override { return nullptr; }
 } debug;
 DISPATCHER<FUNCTION>::INSTALL d_debug(&function_dispatcher, "$debug", &debug);
+/*--------------------------------------------------------------------------*/
+class TRACE_TASK : public DEBUG_TASK {
+public:
+  explicit TRACE_TASK() : DEBUG_TASK(){
+    set_label("$trace");
+  }
+private:
+  std::string eval(CS&, const PARAM_LIST*)const override{ untested();
+    unreachable();
+    return "$$trace";
+  }
+  MGVAMS_TASK* clone()const override {
+    return new TRACE_TASK(*this);
+  }
+  bool has_tr_advance()const override {return true;}
+  bool has_tr_regress()const override { untested();return true;}
+  bool has_tr_accept()const override {return true;}
+  bool has_tr_begin()const override {return true;}
+  bool has_tr_restore()const override {return true;}
+  bool static_code()const override {return false;} // ?
+  bool has_modes()const override {return true;}
+  void make_cc_dev(std::ostream& o)const override {
+    make_cc_dev_(o, "ac_begin");
+    make_cc_dev_(o, "tr_begin");
+    make_cc_dev_(o, "tr_eval");
+    make_cc_dev_(o, "tr_accept");
+    make_cc_dev_(o, "tr_advance");
+    make_cc_dev_(o, "tr_regress");
+    make_cc_dev_(o, "tr_restore");
+    make_cc_dev_(o, "tr_finish");
+    make_cc_dev_(o, "precalc");
+  }
+#if 0 // share with DEBUG?
+  void make_cc_dev_(std::ostream& o, std::string s)const {
+    o__ "void " << label() << s << "(std::string a0";
+    for(size_t i=1; i<num_args(); ++i) {
+      o << ", double a" << i;
+    }
+    o << ") {\n";
+    o______ "a0 = std::string(\"@" << s << ":\") + a0;\n";
+    o______ "fprintf(stdout, a0.c_str()";
+    for(size_t i=1; i<num_args(); ++i) {
+      o << ", a" << i;
+    }
+    o << ");\n";
+    o__ "}\n";
+  }
+#endif
+  std::string code_name()const override{
+    return label();
+  }
+} trace;
+DISPATCHER<FUNCTION>::INSTALL d_trace(&function_dispatcher, "$trace", &trace);
 /*--------------------------------------------------------------------------*/
 }
 /*--------------------------------------------------------------------------*/
